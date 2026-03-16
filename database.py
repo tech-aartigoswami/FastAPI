4340df5c1d17999a59bbe0359724f0e91ac938e7
aarti <tech.aartigoswami@gmail.com>
2026-03-16 18:08:08 +0530

    message

--- database.py
+++ database.py
@@ -1,7 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker , declarative_base
 
-DATABASE_URL="mysql+pymysql://root:root@localhost/my_fastapi_db"
+load_dotenv()
+
+DB_USER=os.getenv("DB_USER")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
+DB_HOST=os.getenv("DB_HOST")  
+DB_PORT=os.getenv("DB_PORT")
+DB_NAME=os.getenv("DB_NAME")
+
+DATABASE_URL=f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine=create_engine(DATABASE_URL)
 
