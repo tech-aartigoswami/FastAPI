371702b17b149d124baba4bc1d1657817040c9cf
aarti <tech.aartigoswami@gmail.com>
2026-03-16 19:19:36 +0530

    Updated FastAPI project and added Docker setup

--- first-fastAPI/main.py
+++ first-fastAPI/main.py
@@ -35,4 +35,27 @@ def test_db():
 def create_user_api(user:UserCreate,db:Session=Depends(get_db)):
     return create_user(db,user)
 
+
+@app.get("/get-all-users",response_model=list[UserResponse])
+def get_users_api(db:Session=Depends(get_db)):
+    return get_users(db)
+
+
+@app.get("/get-users-by-id/{user_id}",response_model=list[UserResponse])
+def get_users_by_id_api(user_id:int,db:Session=Depends(get_db)):
+    return get_user_by_id(db)
+
+
+
+
+@app.delete("/delete-users-by-id/{user_id}")
+def get_users_by_id_api(user_id:int,db:Session=Depends(get_db)):
+    user=delete_user_by_id(db,user_id)
+
+    if not user:
+        raise HTTPException(status_code=404,detail="User Not Found")
     
+    return {"message":"User Deleted Successfully"}
+
+
+
--- first-fastAPI/services/crud.py
+++ first-fastAPI/services/crud.py
@@ -5,6 +5,7 @@ from schema import UserCreate
 import models
 import schema
 
+
 def create_user(db:session,user:schema.UserCreate):
     db_user=User(
         name=user.name,
@@ -15,3 +16,26 @@ def create_user(db:session,user:schema.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def get_users(db:Session):
+    users=db.query(User).all()# select + from user
+    return users
+
+def get_user_by_id(db:Session,user_id:int):
+    user=db.query(User).filter(User.id==user_id).first()
+    return user
+
+
+def get_user_by_id(db:Session,user_id:int):
+    user=db.query(User).filter(User.id==user_id).first()
+    return user
+
+def delete_user_by_id(db:Session,user_id:int):
+    user=db.query(User).filter(User.id==user_id).first()
+    
+    if not user:
+        return None
+    db.delete(user)
+    db.commit()
+    return user
